9de381c9e370205f373b606a8c54304a8236620c
mat-kainos <a@a.com>
2019-06-04 15:24:10 +0200

    + egress rule for internet access

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -11,8 +11,17 @@ resource aws_security_group sec_group {
 resource aws_security_group_rule allow_all_my_cidr_and_internal {
   type        = "ingress"
   from_port   = 0
-  to_port     = 65535
+  to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["${chomp(data.http.my_ip.body)}/32", var.network["vpc_cidr_block"]]
+  security_group_id = aws_security_group.sec_group.id
+}
+
+resource aws_security_group_rule allow_all_outbound {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.sec_group.id
 }
