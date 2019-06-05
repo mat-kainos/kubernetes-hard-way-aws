d9880ed0a6bf889e7d59686249d86e06341bcf25
mat-kainos <a@a.com>
2019-06-05 12:15:46 +0200

    wip kube-apiserver service

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,6 +2,11 @@ output security_group_my_cidr_block {
   value = "${chomp(data.http.my_ip.body)}/32"
 }
 
+output subnet_cidr {
+  value = var.network.subnet_cidr_block
+}
+
+
 output ec2_connection_strings {
   value = "${formatlist(
     "%s: ssh -i ${path.cwd}/${var.private_key_file_name} ubuntu@%s",
