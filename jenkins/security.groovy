f762e2734782efe2af07fdb4659ba9ad114b9da3
mat-kainos <a@a.com>
2019-06-11 11:26:55 +0200

    docker mostly ok, wip jenkinsfile

--- jenkins/security.groovy
+++ jenkins/security.groovy
@@ -6,8 +6,8 @@ import jenkins.security.s2m.AdminWhitelistRule
 
 def instance = Jenkins.getInstance()
 
-def user = new File("/run/secrets/jenkins-user").text.trim()
-def pass = new File("/run/secrets/jenkins-pass").text.trim()
+def user = new File("/tmp/user.sensitive").text.trim()
+def pass = new File("/tmp/pwd.sensitive").text.trim()
 
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 hudsonRealm.createAccount(user, pass)
--- run.sh
+++ run.sh
@@ -2,7 +2,9 @@
 
 cd jenkins
 
-echo "Create new Jenkins user:"
+NAME=kthw-jenkins
+
+echo "Create new Jenkins admin user:"
 read user
 
 echo "Create password for user $user: "
@@ -13,15 +15,18 @@ echo $pwd > pwd.sensitive
 
 echo "Building docker image: kthw-jenkins"
 
-
 AWS_ACCESS_KEY_ID=$(aws --profile default configure get aws_access_key_id)
 AWS_SECRET_ACCESS_KEY=$(aws --profile default configure get aws_secret_access_key)
 
 docker build \
--t kthw-jenkins \
---build-arg AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
---build-arg AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+-t $NAME \
+--build-arg AWS_ACCESS_KEY_ID_ARG=$AWS_ACCESS_KEY_ID \
+--build-arg AWS_SECRET_ACCESS_KEY_ARG=$AWS_SECRET_ACCESS_KEY \
 .
 
+echo "Stopping and removing kthw-jenkins container (if it exists)"
+docker container stop $NAME
+docker container rm $NAME
 
-
+echo "Running kthw-jenkins container"
+docker run -d --name=$NAME -p 8080:8080 -p 50000:50000 $NAME
